2f4e93d9f4cb497f40b1b033303c33a4f0e9eda4
Gerardo Medrano <gma31096@hotmail.com>
2019-07-01 14:36:12 -0500

    Encapsulado en una funcion . mejorado

--- hola.cpp
+++ hola.cpp
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 void reverse(char text[]){
-	for(int i = 0; i<=strlen(text); i++){
+	for(int i = 0; i<=strlen(text); i++)
         printf("%c",text[strlen(text)-i]);   
-    }
+}
 }
 
 int main()
 {
-	//hola
     char text[] = "Hello world!";
     printf("%s\n",text);
 	reverse(text);
