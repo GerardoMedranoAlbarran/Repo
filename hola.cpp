f8617f04018586c2f19b4a9fb0beb5d42821d000
Gerardo Medrano <gma31096@hotmail.com>
2019-07-02 10:31:03 -0500

    patching

--- hola.cpp
+++ hola.cpp
@@ -5,7 +5,20 @@ void reverse(char text[]){
 	for(int i = 0; i<=strlen(text); i++)
         printf("%c",text[strlen(text)-i]);
 }
+void Numeros(int final){
+	for(int i = 1; i<=final;i++){
+		if(i%3 ==0 )
+			printf("Tres");
 
+		if(i%5==0)
+			printf("Cinco");
+		if(i%3 != 0 && i%5 != 0){
+			for(int j = 0; j < i ; j++)
+				printf("%d ",i);
+		}
+		printf("\n");
+	}
+}
 int main()
 {
 	//hola
