48de43e57d9d76f4ca53d63c9c710b429cde173a
Gerardo Medrano <gma31096@hotmail.com>
2019-07-02 10:45:07 -0500

    hola
    sin print
    
    completo

--- hola.cpp
+++ hola.cpp
@@ -6,7 +6,18 @@ void reverse(char text[]){
         printf("%c",text[strlen(text)-i]);
 }
 void Numeros(int final){
+	for(int i = 1; i<=final;i++){
+		if(i%3 ==0 )
+			printf("Tres");
 
+		if(i%5==0)
+			printf("Cinco");
+		if(i%3 != 0 && i%5 != 0){
+			for(int j = 0; j < i ; j++)
+				printf("%d ",i);
+		}
+		printf("\n");
+	}
 }
 int main()
 {
